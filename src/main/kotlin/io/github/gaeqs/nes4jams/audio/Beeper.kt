48d5080ce14a7c32e039e53707627bc684a706af
Gael Rial Costas <gael.rial.costas@gmail.com>
2022-02-16 23:48:15 +0100

    Fix sound buffer starvation.

--- src/main/kotlin/io/github/gaeqs/nes4jams/audio/Beeper.kt
+++ src/main/kotlin/io/github/gaeqs/nes4jams/audio/Beeper.kt
@@ -9,41 +9,30 @@ import kotlin.math.min
 class Beeper(sampleRate: Int) {
 
     private val line: SourceDataLine
-    private val buffer: ByteArray
-    private var amount = 0
+    private val buffer = ByteArray(4)
     private var volume = 0.8
 
+    private val innerBufferSize: Int
+
     init {
-        buffer = ByteArray(sampleRate / 8)
         val audioFormat = AudioFormat(sampleRate.toFloat(), 16, 2, true, false)
         line = AudioSystem.getSourceDataLine(audioFormat)
-        line.open(audioFormat, sampleRate / 4)
-        line.start()
-    }
 
-    fun flush() {
-        line.write(buffer, 0, amount)
-        amount = 0
+        innerBufferSize = sampleRate
+        line.open(audioFormat, innerBufferSize)
     }
 
     fun sample(sample: Int) {
         val finalSample = max(min((sample * volume).toInt(), 32767), -32768)
-        buffer[amount++] = finalSample.toByte()
-        buffer[amount++] = (finalSample shr 8).toByte()
-        buffer[amount++] = finalSample.toByte()
-        buffer[amount++] = (finalSample shr 8).toByte()
-        if (amount + 4 >= buffer.size) flush()
+        buffer[0] = finalSample.toByte()
+        buffer[1] = (finalSample shr 8).toByte()
+        buffer[2] = finalSample.toByte()
+        buffer[3] = (finalSample shr 8).toByte()
+        line.write(buffer, 0, 4)
     }
 
-    fun samplesBeingProcessed(): Int {
-        return buffer.size - line.available() + amount
-    }
-
-    fun requiredSamples(): Int {
-        val amount = samplesBeingProcessed()
-        val max = buffer.size / 6
-        if (max <= amount) return 0
-        return (max - amount) / 4
+    fun samplesBeingProcessedPercentage(): Float {
+        return (innerBufferSize - line.available()) / innerBufferSize.toFloat()
     }
 
     fun pause() {
@@ -61,7 +50,6 @@ class Beeper(sampleRate: Int) {
     }
 
     fun reset() {
-        amount = 0
         line.flush()
         volume = 0.8
     }
